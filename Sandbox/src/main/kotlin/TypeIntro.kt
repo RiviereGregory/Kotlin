da87f8ad9a5ff4b7aec90e8c81bbbeab0c20a61a
RiviereGregory <gregory.riviere@laposte.net>
2021-10-14 20:40:52 +0200

    Défi1 Acquisition d'une monture

--- Sandbox/src/main/kotlin/TypeIntro.kt
+++ Sandbox/src/main/kotlin/TypeIntro.kt
@@ -1,10 +1,12 @@
 const val MAX_EXPERIENCE: Int = 5000
 
 fun main() {
+    var hasSteed = false
     val playerName = "Estragon"
     var experiencePoints = 5
     experiencePoints += 5
     println(experiencePoints)
     println(playerName)
     println(MAX_EXPERIENCE)
+    println("Le joueur à une monture $hasSteed")
 }
