0fb7f523ef1427ba025a95443a9611563cea2352
RiviereGregory <gregory.riviere@laposte.net>
2021-10-21 20:15:56 +0200

    [NyetHack] if/else imbriqué

--- NyetHack/src/main/kotlin/Game.kt
+++ NyetHack/src/main/kotlin/Game.kt
@@ -1,12 +1,17 @@
 fun main() {
     val name = "Madrigal"
     var healthPoints = 89
+    val isBlessed = true
     if (healthPoints == 100) {
         println("$name est en parfaite condtion !")
     } else if (healthPoints >= 90) {
         println("$name a quelques égratinures.")
     } else if (healthPoints >= 75) {
-        println("$name a quelques blessures mineures.!")
+        if(isBlessed){
+            println("$name a quelques blessures mineures, mais se rétablit vite !")
+        }else {
+            println("$name a quelques blessures mineures.")
+        }
     } else if (healthPoints >= 15) {
         println("$name semble mal en point.")
     } else {
