4c5353808c7f10730ab11680eb62ebee58521e7a
RiviereGregory <gregory.riviere@laposte.net>
2021-10-21 20:18:41 +0200

    [NyetHack] if/else opérateur logique

--- NyetHack/src/main/kotlin/Game.kt
+++ NyetHack/src/main/kotlin/Game.kt
@@ -2,14 +2,22 @@ fun main() {
     val name = "Madrigal"
     var healthPoints = 89
     val isBlessed = true
+    val isImmortal = false
+
+    if (isBlessed && healthPoints > 50 || isImmortal) {
+        println("VERTE")
+    } else {
+        println("AUCUNE")
+    }
+
     if (healthPoints == 100) {
         println("$name est en parfaite condtion !")
     } else if (healthPoints >= 90) {
         println("$name a quelques égratinures.")
     } else if (healthPoints >= 75) {
-        if(isBlessed){
+        if (isBlessed) {
             println("$name a quelques blessures mineures, mais se rétablit vite !")
-        }else {
+        } else {
             println("$name a quelques blessures mineures.")
         }
     } else if (healthPoints >= 15) {
