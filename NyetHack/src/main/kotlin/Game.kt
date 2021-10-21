3a65fd5141650eac4265a3c4664bc303524160ff
RiviereGregory <gregory.riviere@laposte.net>
2021-10-21 20:27:20 +0200

    [NyetHack] if/else expression conditionnelle

--- NyetHack/src/main/kotlin/Game.kt
+++ NyetHack/src/main/kotlin/Game.kt
@@ -4,26 +4,27 @@ fun main() {
     val isBlessed = true
     val isImmortal = false
 
-    if (isBlessed && healthPoints > 50 || isImmortal) {
-        println("VERTE")
-    } else {
-        println("AUCUNE")
-    }
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (auraVisible) "VERTE" else "AUCUNE"
+    println(auraColor)
 
-    if (healthPoints == 100) {
-        println("$name est en parfaite condtion !")
+
+    val healthStatus = if (healthPoints == 100) {
+        "est en parfaite condtion !"
     } else if (healthPoints >= 90) {
-        println("$name a quelques égratinures.")
+        "a quelques égratinures."
     } else if (healthPoints >= 75) {
         if (isBlessed) {
-            println("$name a quelques blessures mineures, mais se rétablit vite !")
+            "a quelques blessures mineures, mais se rétablit vite !"
         } else {
-            println("$name a quelques blessures mineures.")
+            "a quelques blessures mineures."
         }
     } else if (healthPoints >= 15) {
-        println("$name semble mal en point.")
+        "semble mal en point."
     } else {
-        println("$name est dans une condition épouvantable !")
+        "est dans une condition épouvantable !"
     }
+// Etat de santé du joueur
+    println("$name $healthStatus")
 
 }
