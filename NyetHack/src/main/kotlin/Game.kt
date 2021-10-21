e1bc574a50b755b3db51adb232b2714a64b71a36
RiviereGregory <gregory.riviere@laposte.net>
2021-10-21 20:32:32 +0200

    [NyetHack] if/else utilisation de plage avec in

--- NyetHack/src/main/kotlin/Game.kt
+++ NyetHack/src/main/kotlin/Game.kt
@@ -9,11 +9,11 @@ fun main() {
     println(auraColor)
 
     val healthStatus = if (healthPoints == 100) "est en parfaite condtion !"
-    else if (healthPoints >= 90) "a quelques égratinures."
-    else if (healthPoints >= 75)
+    else if (healthPoints in 90..99) "a quelques égratinures."
+    else if (healthPoints in 75..89)
         if (isBlessed) "a quelques blessures mineures, mais se rétablit vite !"
         else "a quelques blessures mineures."
-    else if (healthPoints >= 15) "semble mal en point."
+    else if (healthPoints in 15..74) "semble mal en point."
     else "est dans une condition épouvantable !"
 
 // Etat de santé du joueur
