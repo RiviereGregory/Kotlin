6ba562295899457a92f11f7fbcfa0ce764c6579f
RiviereGregory <gregory.riviere@laposte.net>
2021-10-21 20:30:40 +0200

    [NyetHack] if/else supp accolade

--- NyetHack/src/main/kotlin/Game.kt
+++ NyetHack/src/main/kotlin/Game.kt
@@ -8,22 +8,14 @@ fun main() {
     val auraColor = if (auraVisible) "VERTE" else "AUCUNE"
     println(auraColor)
 
+    val healthStatus = if (healthPoints == 100) "est en parfaite condtion !"
+    else if (healthPoints >= 90) "a quelques égratinures."
+    else if (healthPoints >= 75)
+        if (isBlessed) "a quelques blessures mineures, mais se rétablit vite !"
+        else "a quelques blessures mineures."
+    else if (healthPoints >= 15) "semble mal en point."
+    else "est dans une condition épouvantable !"
 
-    val healthStatus = if (healthPoints == 100) {
-        "est en parfaite condtion !"
-    } else if (healthPoints >= 90) {
-        "a quelques égratinures."
-    } else if (healthPoints >= 75) {
-        if (isBlessed) {
-            "a quelques blessures mineures, mais se rétablit vite !"
-        } else {
-            "a quelques blessures mineures."
-        }
-    } else if (healthPoints >= 15) {
-        "semble mal en point."
-    } else {
-        "est dans une condition épouvantable !"
-    }
 // Etat de santé du joueur
     println("$name $healthStatus")
 
