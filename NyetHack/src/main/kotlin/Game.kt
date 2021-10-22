465c5ca1cec5aaae95daf9ed7b25e0b5f01ab03a
RiviereGregory <gregory.riviere@laposte.net>
2021-10-22 19:15:35 +0200

    [NyetHack] fonction extraction de méthode

--- NyetHack/src/main/kotlin/Game.kt
+++ NyetHack/src/main/kotlin/Game.kt
@@ -7,6 +7,19 @@ fun main() {
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
     println(auraVisible)
 
+    val auraColor = defiAuraColeurKarma(healthPoints)
+
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+
+// Etat de santé du joueur
+    println("(Aura : $auraColor)" + "(Béni : ${if (isBlessed) "OUI" else "NON"})")
+    println("$name $healthStatus")
+
+    // Défi : Format de l'Etat configurable Solution trouvé sur le net
+    defiFormatEtatConfigurable(name, healthStatus, healthPoints, auraColor)
+}
+
+private fun defiAuraColeurKarma(healthPoints: Int): String {
     val karma = (Math.pow(Math.random(), (110 - healthPoints) / 100.0) * 20).toInt()
     val auraColor = when (karma) {
         in 16..20 -> "VERT"
@@ -14,7 +27,39 @@ fun main() {
         in 6..10 -> "ORANGE"
         else -> "ROUGE"
     }
+    return auraColor
+}
 
+private fun defiFormatEtatConfigurable(
+    name: String,
+    healthStatus: String,
+    healthPoints: Int,
+    auraColor: String
+) {
+    val statusFormatString = "(HP)(A) -> H"
+    val formatSpecifiers = "HP|H|A".toRegex()
+
+    var result = StringBuffer()
+
+    var i = 0
+    formatSpecifiers.findAll(statusFormatString).forEach { match ->
+        result.append(statusFormatString.substring(i, match.range.start))
+        result.append(
+            when (match.value) {
+                "H" -> "$name $healthStatus"
+                "HP" -> "HP : $healthPoints"
+                "A" -> "Aura : $auraColor"
+                else -> "?"
+            }
+        )
+        i = match.range.last + 1
+    }
+    result.append(statusFormatString.substring(i))
+
+    println("$result")
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
     val healthStatus = when (healthPoints) {
         100 -> "est en parfaite condtion !"
         in 90..99 -> "a quelques égratinures."
@@ -26,29 +71,5 @@ fun main() {
         in 15..74 -> "semble mal en point."
         else -> "est dans une condition épouvantable !"
     }
-
-// Etat de santé du joueur
-    println("(Aura : $auraColor)" + "(Béni : ${if (isBlessed) "OUI" else "NON"})")
-    println("$name $healthStatus")
-
-    // Défi : Format de l'Etat configurable Solution trouvé sur le net
-    val statusFormatString = "(HP)(A) -> H"
-    val formatSpecifiers = "HP|H|A".toRegex()
-
-    var result = StringBuffer()
-
-    var i = 0
-    formatSpecifiers.findAll(statusFormatString).forEach { match ->
-        result.append(statusFormatString.substring(i,match.range.start))
-        result.append(when (match.value) {
-            "H" -> "$name $healthStatus"
-            "HP" -> "HP : $healthPoints"
-            "A" -> "Aura : $auraColor"
-            else -> "?"
-        })
-        i = match.range.last+1
-    }
-    result.append(statusFormatString.substring(i))
-
-    println("$result")
+    return healthStatus
 }
