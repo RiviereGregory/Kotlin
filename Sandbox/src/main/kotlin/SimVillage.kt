9ff76b8d02d67eb70ca476f0fab36a9a0acdb122
RiviereGregory <gregory.riviere@laposte.net>
2021-10-29 17:27:00 +0200

    Argument pour la fonction anonyme

--- Sandbox/src/main/kotlin/SimVillage.kt
+++ Sandbox/src/main/kotlin/SimVillage.kt
@@ -1,8 +1,8 @@
 fun main() {
-    val greetingFunction: () -> String = {
+    val greetingFunction: (String) -> String = { playerName ->
         val currentYear = 2021
-        "Bienvenue à SimVillage, Monsieur le Maire ! (copyright $currentYear)"
+        "Bienvenue à SimVillage, $playerName ! (copyright $currentYear)"
     }
 
-    println(greetingFunction())
+    println(greetingFunction("Guyal"))
 }
