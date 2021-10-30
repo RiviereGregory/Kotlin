f8769aac1763463777dcec5013c383a72724902d
RiviereGregory <gregory.riviere@laposte.net>
2021-10-30 10:53:23 +0200

    [NyetHack] Exception ajout levée d'exception

---
+++ NyetHack/src/main/kotlin/SwordJuggler.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var swordJuggling: Int? = null
+    val isJugglingProficient = (1..3).shuffled().last() == 3
+    if (isJugglingProficient) {
+        swordJuggling = 2
+    }
+
+    proficiencyCheck(swordJuggling)
+    swordJuggling = swordJuggling!!.plus(1)
+    println("Vous jonglez avec $swordJuggling épées!")
+}
+
+fun proficiencyCheck(swordJuggling: Int?) {
+    swordJuggling ?: throw IllegalStateException("Le joueur ne sait pas jongler avec des épées")
+}
