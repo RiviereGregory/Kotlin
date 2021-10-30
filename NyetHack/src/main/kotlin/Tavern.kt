899c023f6fbdbee741f44b89ba232d91b2bf4f2c
RiviereGregory <gregory.riviere@laposte.net>
2021-10-30 10:30:17 +0200

    [NyetHack] null utilisation de l'opérateur coalescence null avec let pour remplacer if/else

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -1,11 +1,9 @@
 fun main() {
     var beverage = readLine()
-
-    if (beverage != null) {
-        beverage = beverage.capitalize()
-    } else {
-        println("Je ne peux pas faire cela sans planter - beverage = null !!!")
-    }
+//    beverage = null
+    beverage?.let {
+        beverage = it.capitalize()
+    } ?: println("Je ne peux pas faire cela sans planter - beverage = null !!!")
 
     val beverageServed: String = beverage ?: "Buttered Ale"
     println(beverageServed)
