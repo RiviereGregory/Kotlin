0fae4eeb3b5d3450d942271f39c6d13df30f92e8
RiviereGregory <gregory.riviere@laposte.net>
2021-11-01 09:30:21 +0100

    [NyetHack] Chaine de caractère utilisation split et déstructuration

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -1,11 +1,15 @@
 const val TAVERN_NAME = "Taernyl's Folly"
 
 fun main() {
-    placeOrder()
+    placeOrder("shandy,Dragon's Breath,5.91")
 }
 
-fun placeOrder() {
+fun placeOrder(menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("Madrigal parle avec $tavernMaster de sa commande")
+
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal achète un(e) $name ($type) à $price."
+    println(message)
 }
