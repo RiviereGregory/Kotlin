88d26aaebf426c1cf28739c19beb9c3a2c66d120
RiviereGregory <gregory.riviere@laposte.net>
2021-11-01 09:40:09 +0100

    [NyetHack] Chaine de caractère utilisation replace et regex

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -4,7 +4,7 @@ fun main() {
     placeOrder("shandy,Dragon's Breath,5.91")
 }
 
-fun placeOrder(menuData: String) {
+private fun placeOrder(menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("Madrigal parle avec $tavernMaster de sa commande")
@@ -12,4 +12,19 @@ fun placeOrder(menuData: String) {
     val (type, name, price) = menuData.split(',')
     val message = "Madrigal achète un(e) $name ($type) à $price."
     println(message)
+
+    val phrase = "Ah, quelle merveillece $name"
+    println("madrigal s'écrie : ${toDragonSpeak(phrase)}")
 }
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
