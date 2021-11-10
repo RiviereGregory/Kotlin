cbe42dc76c9e33978cffda1310b9089ddcdd4fe0
RiviereGregory <gregory.riviere@laposte.net>
2021-11-10 19:21:15 +0100

    [NyetHack] Liste et set LISTE iteration for

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -23,12 +23,9 @@ fun main() {
     }
     placeOrder("shandy,Dragon's Breath,5.91")
     //placeOrder("elixir,Shirley's Temple,4.12")
-    println(patronList)
-    patronList.remove("Eli")
-    patronList.add("Alex")
-    patronList.add(0, "Alex")
-    patronList[0] = "Alexis"
-    println(patronList)
+    for (patron in patronList) {
+        println("Bonjour, $patron")
+    }
 }
 
 fun performPurchase(price: Double): Boolean {
