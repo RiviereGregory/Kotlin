d1fec4d01f6ea1fe9a7f55c669d1da130b34b4e5
RiviereGregory <gregory.riviere@laposte.net>
2021-11-27 10:58:58 +0100

    [NyetHack] CLASS refactor pour utiliser que player

--- NyetHack/src/main/kotlin/Game.kt
+++ NyetHack/src/main/kotlin/Game.kt
@@ -1,53 +1,22 @@
-import kotlin.math.pow
-
 fun main() {
-    var healthPoints = 89
-    val isBlessed = true
-    val isImmortal = false
-
     val player = Player()
 
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    println(auraVisible)
-
-    val auraColor = auraColor(healthPoints)
-
-    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
-
     // Etat de santé du joueur
-    printPlayerStatus(auraColor, isBlessed, player.name, healthStatus)
+    printPlayerStatus(player)
 
     // Défi : Format de l'Etat configurable Solution trouvé sur le net
-    defiFormatEtatConfigurable(player.name, healthStatus, healthPoints, auraColor)
+    defiFormatEtatConfigurable(player)
 
     val drunkLevel = player.castFireball()
     println(drunkLevel(drunkLevel))
 }
 
-private fun printPlayerStatus(
-    auraColor: String,
-    isBlessed: Boolean,
-    name: String,
-    healthStatus: String
-) {
-    println("(Aura : $auraColor)" + "(Béni : ${if (isBlessed) "OUI" else "NON"})")
-    println("$name $healthStatus")
+private fun printPlayerStatus(player: Player) {
+    println("(Aura : ${player.auraColor()})" + "(Béni : ${if (player.isBlessed) "OUI" else "NON"})")
+    println("${player.name} ${player.formatHealthStatus()}")
 }
 
-private fun auraColor(healthPoints: Int) = when ((Math.random().pow((110 - healthPoints) / 100.0) * 20).toInt()) {
-    in 16..20 -> "VERT"
-    in 11..15 -> "VIOLET"
-    in 6..10 -> "ORANGE"
-    else -> "ROUGE"
-}
-
-
-private fun defiFormatEtatConfigurable(
-    name: String,
-    healthStatus: String,
-    healthPoints: Int,
-    auraColor: String
-) {
+private fun defiFormatEtatConfigurable(player: Player) {
     val statusFormatString = "(HP)(A) -> H"
     val formatSpecifiers = "HP|H|A".toRegex()
 
@@ -58,9 +27,9 @@ private fun defiFormatEtatConfigurable(
         result.append(statusFormatString.substring(i, match.range.first))
         result.append(
             when (match.value) {
-                "H" -> "$name $healthStatus"
-                "HP" -> "HP : $healthPoints"
-                "A" -> "Aura : $auraColor"
+                "H" -> "${player.name} ${player.formatHealthStatus()}"
+                "HP" -> "HP : ${player.healthPoints}"
+                "A" -> "Aura : ${player.auraColor()}"
                 else -> "?"
             }
         )
@@ -69,18 +38,6 @@ private fun defiFormatEtatConfigurable(
     result.append(statusFormatString.substring(i))
 
     println("$result")
-}
-
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) = when (healthPoints) {
-    100 -> "est en parfaite condtion !"
-    in 90..99 -> "a quelques égratinures."
-    in 75..89 -> if (isBlessed) {
-        "a quelques blessures mineures, mais se rétablit vite !"
-    } else {
-        "a quelques blessures mineures."
-    }
-    in 15..74 -> "semble mal en point."
-    else -> "est dans une condition épouvantable !"
 }
 
 private fun drunkLevel(drunk: Int) = when (drunk) {
--- NyetHack/src/main/kotlin/Player.kt
+++ NyetHack/src/main/kotlin/Player.kt
@@ -1,9 +1,14 @@
+import kotlin.math.pow
+
 class Player {
     var name = "madrigal"
         get() = field.capitalize()
         private set(value) {
             field = value.trim()
         }
+    var healthPoints = 89
+    val isBlessed = true
+    private val isImmortal = false
 
     fun castFireball(numFireballs: Int = 2): Int {
         println("Apparition d'un verre de Fireball. (x$numFireballs)")
@@ -20,5 +25,24 @@ class Player {
             9 -> 45
             else -> 50
         }
+    }
+
+    fun formatHealthStatus() = when (healthPoints) {
+        100 -> "est en parfaite condtion !"
+        in 90..99 -> "a quelques égratinures."
+        in 75..89 -> if (isBlessed) {
+            "a quelques blessures mineures, mais se rétablit vite !"
+        } else {
+            "a quelques blessures mineures."
+        }
+        in 15..74 -> "semble mal en point."
+        else -> "est dans une condition épouvantable !"
+    }
+
+    fun auraColor() = when ((Math.random().pow((110 - healthPoints) / 100.0) * 20).toInt()) {
+        in 16..20 -> "VERT"
+        in 11..15 -> "VIOLET"
+        in 6..10 -> "ORANGE"
+        else -> "ROUGE"
     }
 }
