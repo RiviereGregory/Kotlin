93eb01b7ee3a3be60e110f2c0edbd19a99bf0033
RiviereGregory <gregory.riviere@laposte.net>
2021-12-24 09:30:12 +0100

    [NyetHack] Object ajout d'une class game pour lancer le jeu avec une singleton

--- NyetHack/src/main/kotlin/fr/riverjach/nyethack/Game.kt
+++ NyetHack/src/main/kotlin/fr/riverjach/nyethack/Game.kt
@@ -1,56 +1,73 @@
 package fr.riverjach.nyethack
 
 fun main() {
-    val player = Player("Madrigal")
-
-    // Etat de santé du joueur
-    printPlayerStatus(player)
-
-    // Test Salle
-    var currentRoom = TownSquare()
-    println(currentRoom.description())
-    println(currentRoom.load())
-
-    // Défi : Format de l'Etat configurable Solution trouvé sur le net
-    defiFormatEtatConfigurable(player)
-
-    val drunkLevel = player.castFireball()
-    println(drunkLevel(drunkLevel))
+    Game.play()
 }
 
-private fun printPlayerStatus(player: Player) {
-    println("(Aura : ${player.auraColor()})" + "(Béni : ${if (player.isBlessed) "OUI" else "NON"})")
-    println("${player.name} ${player.formatHealthStatus()}")
-}
+object Game {
+    private val player = Player("Madrigal")
+    private var currentRoom = TownSquare()
 
-private fun defiFormatEtatConfigurable(player: Player) {
-    val statusFormatString = "(HP)(A) -> H"
-    val formatSpecifiers = "HP|H|A".toRegex()
 
-    var result = StringBuffer()
-
-    var i = 0
-    formatSpecifiers.findAll(statusFormatString).forEach { match ->
-        result.append(statusFormatString.substring(i, match.range.first))
-        result.append(
-            when (match.value) {
-                "H" -> "${player.name} ${player.formatHealthStatus()}"
-                "HP" -> "HP : ${player.healthPoints}"
-                "A" -> "Aura : ${player.auraColor()}"
-                else -> "?"
-            }
-        )
-        i = match.range.last + 1
+    init {
+        println("Bienvenue, aventurier.")
+        player.castFireball()
     }
-    result.append(statusFormatString.substring(i))
 
-    println("$result")
-}
+    fun play() {
+        while (true) {
+            // Test Salle
+            println(currentRoom.description())
+            println(currentRoom.load())
 
-private fun drunkLevel(drunk: Int) = when (drunk) {
-    in 1..10 -> "Pompette"
-    in 11..20 -> "Pinté"
-    in 21..30 -> "Murgé"
-    in 31..40 -> "Imbibé"
-    else -> "Décalqué"
+            // Etat de santé du joueur
+            printPlayerStatus(player)
+
+            // Défi : Format de l'Etat configurable Solution trouvé sur le net
+            defiFormatEtatConfigurable(player)
+
+            val drunkLevel = player.castFireball()
+            println(drunkLevel(drunkLevel))
+
+            print("> Saisissez votre commande :")
+            println("Dernière commande : ${readLine()}")
+        }
+    }
+
+    private fun printPlayerStatus(player: Player) {
+        println("(Aura : ${player.auraColor()})" + "(Béni : ${if (player.isBlessed) "OUI" else "NON"})")
+        println("${player.name} ${player.formatHealthStatus()}")
+    }
+
+    private fun defiFormatEtatConfigurable(player: Player) {
+        val statusFormatString = "(HP)(A) -> H"
+        val formatSpecifiers = "HP|H|A".toRegex()
+
+        var result = StringBuffer()
+
+        var i = 0
+        formatSpecifiers.findAll(statusFormatString).forEach { match ->
+            result.append(statusFormatString.substring(i, match.range.first))
+            result.append(
+                when (match.value) {
+                    "H" -> "${player.name} ${player.formatHealthStatus()}"
+                    "HP" -> "HP : ${player.healthPoints}"
+                    "A" -> "Aura : ${player.auraColor()}"
+                    else -> "?"
+                }
+            )
+            i = match.range.last + 1
+        }
+        result.append(statusFormatString.substring(i))
+
+        println("$result")
+    }
+
+    private fun drunkLevel(drunk: Int) = when (drunk) {
+        in 1..10 -> "Pompette"
+        in 11..20 -> "Pinté"
+        in 21..30 -> "Murgé"
+        in 31..40 -> "Imbibé"
+        else -> "Décalqué"
+    }
 }
