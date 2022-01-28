83fade1c402ad77ae9637cdd7cee8bb9eccea63b
RiviereGregory <gregory.riviere@laposte.net>
2022-01-28 10:15:39 +0100

    SamodelKin Modification nom Activity et mise en forme de la page activity

--- Samodelkin/app/src/main/java/gri/riverjach/samodelkin/NewCharacterActivity.kt
+++ Samodelkin/app/src/main/java/gri/riverjach/samodelkin/NewCharacterActivity.kt
@@ -3,7 +3,7 @@ package gri.riverjach.samodelkin
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class NewCharacterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
