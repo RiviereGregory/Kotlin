6add81071b6e22bb860225bd9a1e38b6961515cc
RiviereGregory <gregory.riviere@laposte.net>
2022-01-28 10:37:32 +0100

    SamodelKin ajout mapping valeur personnage sur interface

--- Samodelkin/app/src/main/java/gri/riverjach/samodelkin/NewCharacterActivity.kt
+++ Samodelkin/app/src/main/java/gri/riverjach/samodelkin/NewCharacterActivity.kt
@@ -1,11 +1,30 @@
 package gri.riverjach.samodelkin
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class NewCharacterActivity : AppCompatActivity() {
+    private var characterData = CharacterGenerator.generate()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nameTextView = findViewById<TextView>(R.id.nameTextView)
+        val raceTextView = findViewById<TextView>(R.id.raceTextView)
+        val dexterityTextView = findViewById<TextView>(R.id.dexterityTextView)
+        val wisdomTextView = findViewById<TextView>(R.id.wisdomTextView)
+        val strengthTextView = findViewById<TextView>(R.id.strengthTextView)
+        val generateButton = findViewById<Button>(R.id.generateButton)
+
+        characterData.run {
+            nameTextView.text = name
+            raceTextView.text = race
+            dexterityTextView.text = dex
+            wisdomTextView.text = wis
+            strengthTextView.text = str
+        }
     }
 }
